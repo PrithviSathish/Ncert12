51550a0a744f6bb05c4f314a6db0c3d361aded71
PrithviSathish <prithvi.mar05@gmail.com>
2021-06-29 12:55:44 +0530

    Added new functions

--- Functions.py
+++ Functions.py
@@ -4,63 +4,48 @@ def fact(n):
     for i in range(1, n + 1):
         f1 *= i
     return f1
-	
+
 
 # Without arg
 def sum1():
-	a = int(input("Enter num 1: "))
-	b = int(input("Enter num 2: "))
-	c = a + b
-	print(c)
-	
+    a = int(input("Enter num 1: "))
+    b = int(input("Enter num 2: "))
+    c = a + b
+    print(c)
+
 
 # With arg
 def sum2(a, b):
-	print(a + b)
-	
-	
+    print(a + b)
+
+
 # Without arg but with return type
 def sum3():
-	a = int(input("Enter num 1: "))
-	b = int(input("Enter num 2: "))
-	return a + b
-	
+    a = int(input("Enter num 1: "))
+    b = int(input("Enter num 2: "))
+    return a + b
+
 
 # With arg and return type
 def sum2(a, b):
-	return a + b
-	
-
-'''
------------ Factorial -------------
-x = int(input("Enter the first value: "))
-y = int(input("Enter the second value: "))
-u = int(input("Enter the third value: "))
-v = int(input("Enter the fourth value: "))
-
-z = (fact(x) + fact(y)) / (fact(u) + fact(v))
-print(z)
+    return a + b
 
 
------------- Sum of 2 numbers --------------
+# subtract with return
+def diff(a, b):
+    return a - b
 
-# For sum without arg
-sum1()
 
-# For sum with arg
-x = int(input("Enter the first num: "))
-y = int(input("Enter the second num: "))
-sum(x + y)
+# sum of n numbers without return
+def sum_natural(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    print("sum of first", n, "natural numbers is:", sum)
 
-# For sum without arg but with return type
-c = sum3()
-print(c)
-
-# For sum with arg and return type
-x = int(input("Enter the first num: "))
-y = int(input("Enter the second num: "))
-z = sum(x, y)
-print(z)
-
-'''
-
+# Find the mean of the list
+def myMean(l):
+    s = 0
+    for x in l:
+        s += x
+    return s / len(l)
---
+++ implement.py
@@ -0,0 +1,50 @@
+import Functions
+
+'''
+----------- Factorial -------------
+x = int(input("Enter the first value: "))
+y = int(input("Enter the second value: "))
+u = int(input("Enter the third value: "))
+v = int(input("Enter the fourth value: "))
+
+z = (fact(x) + fact(y)) / (fact(u) + fact(v))
+print(z)
+
+
+------------ Sum of 2 numbers --------------
+
+# For sum without arg
+sum1()
+
+# For sum with arg
+x = int(input("Enter the first num: "))
+y = int(input("Enter the second num: "))
+sum(x + y)
+
+# For sum without arg but with return type
+c = sum3()
+print(c)
+
+# For sum with arg and return type
+x = int(input("Enter the first num: "))
+y = int(input("Enter the second num: "))
+z = sum(x, y)
+print(z)
+
+------------------- using it as a library -----------------
+
+x = int(input("Enter the first num: "))
+y = int(input("Enter the second num: "))
+
+z = Functions.diff(x, y)
+print(z)
+
+------------ sum of natural numbers -----------
+n = int(input("Enter the value of n: "))
+Functions.sum_natural(5)
+
+----------- Mean of a given list -----------
+mean = Functions.myMean([5, 10, 15])
+print(mean)
+'''
+
