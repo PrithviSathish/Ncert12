6024147df46111aeefd021a48dd16567bca46e4e
Prithvi Sathish <prithvi.mar05@gmail.com>
2021-07-19 14:10:31 +0530

    Added abinav's method

--- readFile.py
+++ readFile.py
@@ -1,1 +1,4 @@
 print("number of characters in the file: ", len(open(input("Enter the file name: "), "r").read()))
+
+# Alternate method - proposed by Abinav
+print("number of characters in the file: ", open(input("Enter the file name: "), "a").tell())
